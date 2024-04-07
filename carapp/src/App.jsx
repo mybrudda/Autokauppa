6ed660054967bd2b54d0c7f652db20b19a7bfc76
mybrudda <149365096+mybrudda@users.noreply.github.com>
2024-04-07 13:52:42 +0300

    appbar added from mui

--- carapp/src/App.jsx
+++ carapp/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Carlist from './components/Carlist'
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
+import './App.css';
+import Carlist from './components/Carlist';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Carlist></Carlist>
+      
+      <AppBar position="fixed" style={{ height: '70px' }}> 
+        <Toolbar style={{ justifyContent: 'center' }}> 
+          <Typography variant="h4" component="div">
+            CarShop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Toolbar />
+      
+      <div style={{ marginBottom: '40%' }}> {/* Adding margin-top to position Carlist higher */}
+        <Carlist />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
