3a823b1d08535f33a58cbf80489d8cd6bf8ad19a
mybrudda <149365096+mybrudda@users.noreply.github.com>
2024-04-07 13:22:37 +0300

    Ag-grid added

--- carapp/src/components/Carlist.jsx
+++ carapp/src/components/Carlist.jsx
@@ -1,18 +1,43 @@
+import 'ag-grid-community/styles/ag-grid.css';
+import "ag-grid-community/styles/ag-theme-material.css";
+import { AgGridReact } from 'ag-grid-react';
 import React, { useEffect, useState } from 'react';
 
-
-
-export default function Carlist(){
+export default function Carlist() {
     const [cars, setCars] = useState([]);
 
-    useEffect(() => fetchData(), []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const fetchData = () => {
         fetch('https://carrestservice-carshop.rahtiapp.fi/cars')
-        .then(response => response.json())
-        .then(data => setCars(data._embedded.cars))
-    }
+            .then(response => response.json())
+            .then(data => setCars(data._embedded.cars))
+            .catch(error => console.error('Error fetching data:', error));
+    };
+
+    const columnDefs = [
+        { headerName: 'Brand', field: 'brand', width: 200 }, // Width set to 200 for all columns
+        { headerName: 'Model', field: 'model', width: 200 },
+        { headerName: 'Color', field: 'color', width: 200 },
+        { headerName: 'Fuel', field: 'fuel', width: 150 },
+        { headerName: 'Year', field: 'modelYear', width: 150 },
+        { headerName: 'Price', field: 'price', width: 150 }
+    ];
+
     return (
-        <div></div>
+        <div>
+            <div className="ag-theme-material" style={{ height: '500px', width: '1100px' }}>
+            <AgGridReact
+                rowSelection="single"
+                animateRows={true}
+                rowData={cars}
+                columnDefs={columnDefs}
+                pagination={true}
+            />
+            </div>
+            
+        </div>
     );
 }
