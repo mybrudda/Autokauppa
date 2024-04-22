2bc1834377f2dbe4176dfdfebbb924fb617ba2cd
mybrudda <149365096+mybrudda@users.noreply.github.com>
2024-04-22 21:52:36 +0300

    Revert "new update"
    This reverts commit d4b076f5b74ee088c63bea390ab9aee500975975.

--- carapp/vite.config.js
+++ carapp/vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactapp/',
   plugins: [react()],
 })
