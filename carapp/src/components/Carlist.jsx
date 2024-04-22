639ed205c48192e1180004c8ac22fbd83cab4c20
mybrudda <149365096+mybrudda@users.noreply.github.com>
2024-04-22 22:33:06 +0300

    Reapply "Edit functionality added"
    This reverts commit 73b98e4b5b3a76219fad1fd07d243f4b7493f377.

--- carapp/src/components/Carlist.jsx
+++ carapp/src/components/Carlist.jsx
@@ -3,10 +3,11 @@ import "ag-grid-community/styles/ag-theme-material.css";
 import { AgGridReact } from "ag-grid-react";
 import React, { useEffect, useState } from "react";
 import Addcar from "./Addcar";
+import EditCar from "./EditCar"; // Import EditCar component
 
 export default function Carlist() {
   const [cars, setCars] = useState([]);
-  const [gridApi, setGridApi] = useState(null); 
+  const [gridApi, setGridApi] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -19,23 +20,47 @@ export default function Carlist() {
       .catch((error) => console.error("Error fetching data:", error));
   };
 
+  const updateCar = (car, link) => {
+    fetch(link, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(car),
+    })
+   .then(res => fetchData())
+   .catch(err => console.error(err))
+  }
+
   const columnDefs = [
-    { headerName: "Brand", field: "brand", width: 200 },
-    { headerName: "Model", field: "model", width: 200 },
-    { headerName: "Color", field: "color", width: 200 },
-    { headerName: "Fuel", field: "fuel", width: 150 },
-    { headerName: "Year", field: "modelYear", width: 150 },
-    { headerName: "Price", field: "price", width: 150 },
+    { headerName: "Brand", field: "brand", width: 150, editable: true }, 
+    { headerName: "Model", field: "model", width: 150, editable: true },
+    { headerName: "Color", field: "color", width: 150, editable: true },
+    { headerName: "Fuel", field: "fuel", width: 150, editable: true },
+    { headerName: "Year", field: "modelYear", width: 150, editable: true },
+    { headerName: "Price", field: "price", width: 150, editable: true },
+    {
+      width: 100,
+      headerName: "Edit",
+      cellRenderer: (props) => (
+        <div>
+          {/* Render EditCar component with props.original as car prop */}
+          <EditCar updateCar={updateCar} car={props.data}></EditCar>
+        </div>
+      ),
+    },
   ];
 
+
+
   const onGridReady = (params) => {
-    setGridApi(params.api); 
+    setGridApi(params.api);
   };
 
   const handleDeleteRow = () => {
-    const selectedRows = gridApi.getSelectedRows(); 
+    const selectedRows = gridApi.getSelectedRows();
     if (selectedRows.length === 1) {
-      const carId = selectedRows[0]._links.self.href.split("/").pop(); 
+      const carId = selectedRows[0]._links.self.href.split("/").pop();
       fetch(`https://carrestservice-carshop.rahtiapp.fi/cars/${carId}`, {
         method: "DELETE",
       })
@@ -54,23 +79,22 @@ export default function Carlist() {
   };
 
   const saveCar = (car) => {
-    
-    fetch('https://carrestservice-carshop.rahtiapp.fi/cars', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(car)
+    fetch("https://carrestservice-carshop.rahtiapp.fi/cars", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(car),
     })
-    .then(res => fetchData())
-    .catch(err => console.error(err));
-}
+      .then((res) => fetchData())
+      .catch((err) => console.error(err));
+  };
 
-
+  
   return (
     <div>
       <div style={{ margin: "20px" }}>
-        <Addcar saveCar={saveCar}/>
+        <Addcar saveCar={saveCar} />
       </div>
       <div
         className="ag-theme-material"
@@ -88,6 +112,10 @@ export default function Carlist() {
           columnDefs={columnDefs}
           pagination={true}
           onGridReady={onGridReady}
+          // Enable editing
+          defaultColDef={{
+            editable: true,
+          }}
         />
       </div>
     </div>
